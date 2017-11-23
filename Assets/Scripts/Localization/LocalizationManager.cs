8da5c8df8475a0399117655846b7c25969091593
Toyan Green <toyangreen@gmail.com>
2017-11-23 13:23:23 +0000

    Multi language support WIP.

---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LocalizationManager : MonoBehaviour {
+
+    private const string MISSING_STRING_TEXT = "No localization found"
+    private Dictionary<string, string> localizedTextDictionary;
+
+	public void LoadLocalizedText(string filename)
+    {
+        localizedTextDictionary = new Dictionary<string, string>();
+        string filePath = Path.Combine(Application.streamingAssetsPath, filename);
+        if (File.Exists(filePath))
+        {
+            string jsonString = File.ReadAllText(filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(jsonString);
+
+            for (int i = 0; i < loadedData.Items.Length; i++)
+            {
+                localizedTextDictionary.Add(loadedData.Items[i].key, loadedData.Items[i].value);
+            }
+
+        } else
+        {
+            Debug.LogError("Language file not found.");
+        }
+    }
+
+    public string GetLocalizedValue(string key)
+    {
+        string ret;
+        if(!localizedTextDictionary.TryGetValue(key, out ret))
+        {
+            ret = MISSING_STRING_TEXT;
+        }
+        return ret;
+    }
+}
